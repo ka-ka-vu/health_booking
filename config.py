3a2fd457e816dbe9ad10c2dfd8374bc8566fe560
ka ka vu <kka81049@gmail.com>
2026-06-15 09:55:23 +0700

    security update

--- config.py
+++ config.py
@@ -6,4 +6,7 @@ load_dotenv()
 MONGO_URI = os.getenv("MONGO_URI")
 
 EMAIL_ADDRESS = os.getenv("EMAIL_ADDRESS")
+
 EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
+
+SECRET_KEY = os.getenv("SECRET_KEY")
